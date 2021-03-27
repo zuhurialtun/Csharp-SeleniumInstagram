0cba0e3f728a80ba95bf8e9a9576a1b7648afbb8
Zuhuri Altun <68850412+zuhurialtun@users.noreply.github.com>
2021-03-27 17:55:03 +0300

    Add files via upload

---
+++ İnstagramBotZuhuriAltun/İnstagramBot1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace İnstagramBot1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.lbl_unfollow = new System.Windows.Forms.ListBox();
+            this.lbl_unfollowCount = new System.Windows.Forms.Label();
+            this.btn_find = new System.Windows.Forms.Button();
+            this.cb_loginAuto = new System.Windows.Forms.CheckBox();
+            this.lbl_takipci = new System.Windows.Forms.ListBox();
+            this.lbl_takip = new System.Windows.Forms.ListBox();
+            this.lbl_followerCount = new System.Windows.Forms.Label();
+            this.lbl_followCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(108, 15);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(174, 20);
+            this.tb_username.TabIndex = 0;
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(108, 42);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(174, 20);
+            this.tb_password.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kullanıcı Adı: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Şifre: ";
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.Location = new System.Drawing.Point(317, 45);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(270, 23);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "Giriş Yap";
+            this.btn_login.UseVisualStyleBackColor = false;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // lbl_unfollow
+            // 
+            this.lbl_unfollow.FormattingEnabled = true;
+            this.lbl_unfollow.Location = new System.Drawing.Point(12, 104);
+            this.lbl_unfollow.Name = "lbl_unfollow";
+            this.lbl_unfollow.Size = new System.Drawing.Size(270, 342);
+            this.lbl_unfollow.TabIndex = 7;
+            // 
+            // lbl_unfollowCount
+            // 
+            this.lbl_unfollowCount.AutoSize = true;
+            this.lbl_unfollowCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_unfollowCount.Location = new System.Drawing.Point(12, 462);
+            this.lbl_unfollowCount.Name = "lbl_unfollowCount";
+            this.lbl_unfollowCount.Size = new System.Drawing.Size(152, 24);
+            this.lbl_unfollowCount.TabIndex = 9;
+            this.lbl_unfollowCount.Text = "Takip Etmeyen =";
+            // 
+            // btn_find
+            // 
+            this.btn_find.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_find.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_find.Location = new System.Drawing.Point(12, 75);
+            this.btn_find.Name = "btn_find";
+            this.btn_find.Size = new System.Drawing.Size(270, 23);
+            this.btn_find.TabIndex = 10;
+            this.btn_find.Text = "Takip Etmeyenleri Bul";
+            this.btn_find.UseVisualStyleBackColor = false;
+            this.btn_find.Click += new System.EventHandler(this.btn_find_Click);
+            // 
+            // cb_loginAuto
+            // 
+            this.cb_loginAuto.AutoSize = true;
+            this.cb_loginAuto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cb_loginAuto.Location = new System.Drawing.Point(317, 15);
+            this.cb_loginAuto.Name = "cb_loginAuto";
+            this.cb_loginAuto.Size = new System.Drawing.Size(160, 21);
+            this.cb_loginAuto.TabIndex = 11;
+            this.cb_loginAuto.Text = "Doğrulama İçin Bekle";
+            this.cb_loginAuto.UseVisualStyleBackColor = true;
+            // 
+            // lbl_takipci
+            // 
+            this.lbl_takipci.FormattingEnabled = true;
+            this.lbl_takipci.Location = new System.Drawing.Point(577, 104);
+            this.lbl_takipci.Name = "lbl_takipci";
+            this.lbl_takipci.Size = new System.Drawing.Size(270, 342);
+            this.lbl_takipci.TabIndex = 13;
+            // 
+            // lbl_takip
+            // 
+            this.lbl_takip.FormattingEnabled = true;
+            this.lbl_takip.Location = new System.Drawing.Point(301, 104);
+            this.lbl_takip.Name = "lbl_takip";
+            this.lbl_takip.Size = new System.Drawing.Size(270, 342);
+            this.lbl_takip.TabIndex = 12;
+            // 
+            // lbl_followerCount
+            // 
+            this.lbl_followerCount.AutoSize = true;
+            this.lbl_followerCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_followerCount.Location = new System.Drawing.Point(573, 462);
+            this.lbl_followerCount.Name = "lbl_followerCount";
+            this.lbl_followerCount.Size = new System.Drawing.Size(139, 24);
+            this.lbl_followerCount.TabIndex = 15;
+            this.lbl_followerCount.Text = "Takipçi Sayısı =";
+            // 
+            // lbl_followCount
+            // 
+            this.lbl_followCount.AutoSize = true;
+            this.lbl_followCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_followCount.Location = new System.Drawing.Point(297, 462);
+            this.lbl_followCount.Name = "lbl_followCount";
+            this.lbl_followCount.Size = new System.Drawing.Size(125, 24);
+            this.lbl_followCount.TabIndex = 14;
+            this.lbl_followCount.Text = "Takip Sayısı =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 532);
+            this.Controls.Add(this.lbl_takipci);
+            this.Controls.Add(this.lbl_takip);
+            this.Controls.Add(this.lbl_followerCount);
+            this.Controls.Add(this.lbl_followCount);
+            this.Controls.Add(this.btn_find);
+            this.Controls.Add(this.lbl_unfollowCount);
+            this.Controls.Add(this.lbl_unfollow);
+            this.Controls.Add(this.cb_loginAuto);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_username);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.ListBox lbl_unfollow;
+        private System.Windows.Forms.Label lbl_unfollowCount;
+        private System.Windows.Forms.Button btn_find;
+        private System.Windows.Forms.CheckBox cb_loginAuto;
+        private System.Windows.Forms.ListBox lbl_takipci;
+        private System.Windows.Forms.ListBox lbl_takip;
+        private System.Windows.Forms.Label lbl_followerCount;
+        private System.Windows.Forms.Label lbl_followCount;
+    }
+}
+
---
+++ İnstagramBotZuhuriAltun/İnstagramBot1/Form1.cs
@@ -0,0 +1,207 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Collections;
+
+namespace İnstagramBot1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Takip ve Takipçilerin çekilmesi sayıya göre değişkenlik gösterebilir lütfen listeler doldurulana kadar bekleyiniz...");
+            if (cb_loginAuto.Checked == true) MessageBox.Show("Hesaba giriş sağlandıktan sonra doğrulama kodunu giriniz ve uygulamada çıkan soruyu onaylayınız...");
+            string tName = tb_username.Text;
+            string tPass = tb_password.Text;
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.instagram.com");
+            Thread.Sleep(5000);
+            IWebElement username = driver.FindElement(By.Name("username"));
+            IWebElement password = driver.FindElement(By.Name("password"));
+            IWebElement login = driver.FindElement(By.CssSelector(".sqdOP.L3NKy.y3zKF"));
+            username.SendKeys(tName);
+            password.SendKeys(tPass);
+            login.Click();
+            if (cb_loginAuto.Checked==true)
+            {
+                DialogResult secim = MessageBox.Show("Doğrulama İşlemi Tamamlandı Mı? ","",MessageBoxButtons.YesNo,MessageBoxIcon.Question,MessageBoxDefaultButton.Button2);
+                if (secim == DialogResult.Yes)
+                {
+                    Thread.Sleep(1000);
+                }
+                else
+                {
+                    driver.Navigate().GoToUrl("https://www.instagram.com");
+                }
+            }
+            else
+            {
+                Thread.Sleep(4000);
+            }
+            Thread.Sleep(2000);
+            driver.Navigate().GoToUrl("https://www.instagram.com/"+ tName.ToString());
+            // follow çek //////////////////////////////////////////////////////////////////////////////
+            Thread.Sleep(6000);
+            IWebElement followC = driver.FindElement(By.CssSelector("#react-root > section > main > div > header > section > ul > li:nth-child(3) > a > span"));
+            string XfollowC = followC.Text;
+            XfollowC.Replace('.',' ');
+            XfollowC.Trim();
+            double followCo = Convert.ToDouble(XfollowC);
+            IWebElement follow = driver.FindElement(By.CssSelector("#react-root > section > main > div > header > section > ul > li:nth-child(3) > a > span"));
+            follow.Click();
+            Thread.Sleep(1500);
+            // sayfayı kaydırma javascript //////////////////////////////////////////////////////////////////////////////
+            string jscommand = "" +
+                "sayfa = document.querySelector('.isgrP');" +
+                "sayfa.scrollTo(0,sayfa.scrollHeight);";
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript(jscommand);
+            Thread.Sleep(500);
+            ArrayList followCou = new ArrayList();
+            while (true)
+            {
+                for (int i = 0; i < (followCo/12); i++)
+                {
+                    js.ExecuteScript(jscommand);
+                    Thread.Sleep(250);
+                }
+                js.ExecuteScript(jscommand);
+                Thread.Sleep(250);
+                try
+                {
+                    IReadOnlyCollection<IWebElement> followNamesC = driver.FindElements(By.CssSelector(".FPmhX.notranslate._0imsa"));
+                    foreach (IWebElement item in followNamesC)
+                    {
+                        followCou.Add(item.Text);
+                    }
+                }
+                catch (Exception h)
+                {
+                    MessageBox.Show(h.Message);
+                }
+                if (double.Parse(followCou.Count.ToString()) == (followCo-1) || double.Parse(followCou.Count.ToString()) == (followCo) || double.Parse(followCou.Count.ToString()) == (followCo + 1))
+                {
+                    //MessageBox.Show("Takip Listesi Oluşturuldu...");
+                    break;
+                }
+                followCou.Clear();
+            }
+            // sayfayı kaydırma javascript //////////////////////////////////////////////////////////////////////////////
+            IReadOnlyCollection<IWebElement> followNames = driver.FindElements(By.CssSelector(".FPmhX.notranslate._0imsa"));
+            foreach (IWebElement item in followNames)
+            {
+                lbl_takip.Items.Add(item.Text);
+            }
+            Thread.Sleep(250);
+            lbl_followCount.Text = "Takip Sayısı =" + lbl_takip.Items.Count;
+            
+            // follower çek //////////////////////////////////////////////////////////////////////////////
+            driver.Navigate().GoToUrl("https://www.instagram.com/" + tName.ToString());
+            Thread.Sleep(6000);
+            IWebElement followerC = driver.FindElement(By.CssSelector("#react-root > section > main > div > header > section > ul > li:nth-child(2) > a > span"));
+            string XfollowerC = followerC.Text;
+            XfollowerC.Replace(',',' ');
+            XfollowC.Trim();
+            Double followerCo = Convert.ToDouble(XfollowerC);
+            IWebElement followers = driver.FindElement(By.CssSelector("#react-root > section > main > div > header > section > ul > li:nth-child(2) > a"));
+            followers.Click();
+            Thread.Sleep(1000);
+            string jscommand2 = "" +
+               "sayfa2 = document.querySelector('.isgrP');" +
+               "sayfa2.scrollTo(0,sayfa2.scrollHeight);";
+            IJavaScriptExecutor js2 = (IJavaScriptExecutor)driver;
+            js2.ExecuteScript(jscommand2);
+            Thread.Sleep(500);
+            ArrayList followerCou = new ArrayList();
+            while (true)
+            {
+                for (int i = 0; i < (followerCo / 12); i++)
+                {
+                    js2.ExecuteScript(jscommand2);
+                    Thread.Sleep(250);
+                }
+                js2.ExecuteScript(jscommand2);
+                Thread.Sleep(250);
+                IReadOnlyCollection<IWebElement> followersNamesC = driver.FindElements(By.CssSelector(".FPmhX.notranslate._0imsa"));
+                try
+                {
+                    foreach (IWebElement item in followersNamesC)
+                    {
+                        followerCou.Add(item.Text);
+                    }
+                }
+                catch (Exception h)
+                {
+                    MessageBox.Show(h.Message);
+                }
+                if (double.Parse(followerCou.Count.ToString()) == (followerCo - 1) || double.Parse(followerCou.Count.ToString()) == (followerCo) || double.Parse(followerCou.Count.ToString()) == (followerCo + 1))
+                {
+                    break;
+                }
+                followerCou.Clear();
+            }
+            IReadOnlyCollection<IWebElement> followersNames = driver.FindElements(By.CssSelector(".FPmhX.notranslate._0imsa"));
+            foreach (IWebElement item in followersNames)
+            {
+                lbl_takipci.Items.Add(item.Text);
+            }
+            Thread.Sleep(250);
+            lbl_followerCount.Text = "Takipçi Sayısı =" + lbl_takipci.Items.Count;
+
+            driver.Dispose();
+        }
+
+        private void btn_find_Click(object sender, EventArgs e)
+        {
+            ArrayList takipliste = new ArrayList();
+            ArrayList takipciliste = new ArrayList();
+            ArrayList unfolllowliste = new ArrayList();
+
+            foreach (string item in lbl_takip.Items)
+            {
+                takipliste.Add(item);
+            }
+            foreach (string item in lbl_takipci.Items)
+            {
+                takipciliste.Add(item);
+            }
+            foreach (string item in takipliste)
+            {
+                if (takipciliste.Contains(item))
+                {
+
+                }
+                else
+                {
+                    if (unfolllowliste.Contains(item))
+                    {
+
+                    }
+                    else
+                    {
+                        unfolllowliste.Add(item);
+                    }
+                }
+            }
+            foreach (string item in unfolllowliste)
+            {
+                lbl_unfollow.Items.Add(item);
+            }
+            lbl_unfollowCount.Text = "Takip Etmeyen = "+lbl_unfollow.Items.Count;
+        }
+    }
+}
